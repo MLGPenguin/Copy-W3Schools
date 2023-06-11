2c9a210735b6eb78c4bcbbda74e8a7050cd6eedc
MLGPenguin <mlgpenguboi@gmail.com>
2023-06-12 00:10:40 +1000

    Move Secondary Navbar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
     import "../app.css"
     import 'iconify-icon'
+    import { goto } from '$app/navigation';
+	import tutorials from '$lib/tutorials.json';
+
+  const languages = tutorials.map((tute) => { return tute.topic });
 </script>
 
 <nav class="p-4">
+    <button on:click={() => goto('/')} class="bg-green-200">Home</button>
     <a> Tutorials </a>
     <a> Exercises </a>
     <a> Services </a>
 </nav>
-
+<div class="text-white bg-[#282A35]">
+    {#each languages as lang (lang)}
+      <button on:click={() => goto(`/${lang}?Home`)} class="px-4 py-2 hover:bg-zinc-900 text-sm tracking-wide"> 
+        {lang} 
+      </button>
+    {/each}
+</div>
 
 <slot />
