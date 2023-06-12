03ac97a154921c4de8687e60f8f71f90f337535f
MLGPenguin <mlgpenguboi@gmail.com>
2023-06-12 15:01:01 +1000

    Left side of HTML demo

---
+++ src/lib/components/PillButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let content: string;
+    export let properties: string = "";
+    export let onClick: (() => void) | null = null;
+
+</script>
+
+    <button 
+        on:click={onClick} 
+        class={`text-white ${properties} rounded-full m-4 w-[13rem] h-12`}>{content}
+    </button> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import PillButton from "$lib/components/PillButton.svelte";
 
   let searchinput: string
 
@@ -17,7 +18,7 @@
     <h1 class="text-white py-4 text-7xl">Learn to Code</h1>
     <h2 class="text-rose-200 text-2xl py-4"> With the world's largest web developer site. </h2>
     <br>
-    <form class="w-[80vwmin]">
+    <form class="w-[80vwmin] inline-flex">
       <input bind:value={searchinput} placeholder="Search our tutorials, e.g. HTML" 
         class="w-[24rem] rounded-l-full px-4 py-3 font-bold placeholder-gray-500 align-middle " />
 
@@ -27,7 +28,7 @@
     <div class="p-4" />
     <a class="text-white text-2xl underline">Not Sure Where To Begin?</a>
     <div class='p-12' /> 
-    <div id="HTML Showcase" class=bg-green-200>
+    <div id="HTML Showcase" class=bg-green-100>
       <svg viewBox="0 0 100 100" height=70 width=100% preserveAspectRatio="none">
         <path style="fill:#282A35;" d=" 
           M 0,0  
@@ -35,7 +36,19 @@
           C 35,150 35,0 0,100z " />
       </svg>
       <div class="h-12"></div>
-
+      <div id="Container" class="flex w-[80%] font-semibold">
+        <div id="Left" class='w-[50%] items-center flex flex-col'>
+          <h1 class="text-8xl my-4">HTML</h1>
+          <h4 class="my-4">The language for building web pages</h4>
+          <PillButton content="Learn HTML" properties="bg-green-500" />
+          <PillButton content="Video Tutorial" properties="bg-yellow-200 text-black"/>
+          <PillButton content="HTML Reference" properties="bg-black "/>
+          <PillButton content="Get Certified" properties="bg-red-200 text-black"/>
+        </div>
+        <div id="Right" class="w-[50%] bg-gray-200 shadow-2xl">
+          <h1 class="text-3xl text-left m-4">HTML Example: </h1>
+        </div>
+      </div>
     </div>    
 </center>
 </div>
